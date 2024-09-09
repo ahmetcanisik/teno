f7facde4a991f0eabcd5f3ce66b2a1235e3d61fb
ahmetcanisik <can.isik.business@gmail.com>
2024-09-09 17:28:22 +0300

    updated to 0.0.6

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
-var _Teno_CONFIG;
 import parcol from "parcol";
 export class Teno {
+    #CONFIG;
     constructor(config) {
-        _Teno_CONFIG.set(this, void 0);
-        __classPrivateFieldSet(this, _Teno_CONFIG, config, "f");
+        this.#CONFIG = config;
     }
     log(...lines) {
-        const v = `~d ${__classPrivateFieldGet(this, _Teno_CONFIG, "f").verticalLineIcon}~`;
+        const v = `~d ${this.#CONFIG.verticalLineIcon}~`;
         let maxLength = 0;
         let result = "";
         // Find the longest line length
@@ -29,7 +17,7 @@ export class Teno {
             }
         });
         // Calculate divider and space values
-        const divider = `~d ${__classPrivateFieldGet(this, _Teno_CONFIG, "f").seperatorIcon}~`.repeat(maxLength);
+        const divider = `~d ${this.#CONFIG.seperatorIcon}~`.repeat(maxLength);
         const space = ' '.repeat(maxLength - 2);
         // Create result text
         result += `\n${divider}\n${v}${space}${v}\n`;
@@ -41,7 +29,6 @@ export class Teno {
         console.log(parcol.pit(result));
     }
 }
-_Teno_CONFIG = new WeakMap();
 export default new Teno({
     verticalLineIcon: "|",
     seperatorIcon: "-"
